7c4cc294beb087cf23520da42fafbfbbf0755e2a
Platon2011 <152201014+Platon2011@users.noreply.github.com>
2023-12-09 16:48:39 +0200

    Update TicTacToe.cpp
    Last Function

--- TicTacToee/TicTacToe.cpp
+++ TicTacToee/TicTacToe.cpp
@@ -117,6 +117,17 @@ void SetUp(char field[][3], int size) {
 
     clearField(size, field);
 }
+bool IsAllBusy(int size, char field[][3]) {
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (field[i][j] == ' ') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 int main() {
 
 
